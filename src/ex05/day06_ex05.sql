b6663ed88c6c9f44578a41dcece8f257914322ec
HalutaDanil <halutadanil@icloud.com>
2026-05-12 17:09:21 +0300

    chore: clean up school files and prepare for GitHub
    - Remove School 21 LICENSE and task descriptions
    - Remove empty template directories
    - Update author information to HalutaDanil
    - Add new project README
    - Rename branch to main

--- src/ex05/day06_ex05.sql
+++ src/ex05/day06_ex05.sql
@@ -1,9 +1,12 @@
-comment on table person_discounts is 'discount calculation table';
-
-comment on column person_discounts.id is 'unique number to identify the record';
-
-comment on column person_discounts.person_id is 'persons associated number';
-
-comment on column person_discounts.discount is 'individual discount';
-
-comment on column person_discounts.pizzeria_id is 'pizzerias associated number';
+-- comment on table person_discounts is 'discount calculation table';
+--
+-- comment on column person_discounts.id is 'unique number to identify the record';
+--
+-- comment on column person_discounts.person_id is 'persons associated number';
+--
+-- comment on column person_discounts.discount is 'individual discount';
+--
+-- comment on column person_discounts.pizzeria_id is 'pizzerias associated number';
+SELECT count(*) = 5 AS check
+FROM pg_description
+WHERE objoid = 'person_discounts'::regclass
--- src/ex06/day06_ex06.sql
+++ src/ex06/day06_ex06.sql
@@ -1,7 +1,12 @@
-create sequence seq_person_discounts start 1;
-
-alter table person_discounts
-alter column id set default nextval('seq_person_discounts');
-
-select
-    setval('seq_person_discounts', (select count(*) + 1 from person_discounts));
+-- create sequence seq_person_discounts start 1;
+--
+-- alter table person_discounts
+-- alter column id set default nextval('seq_person_discounts');
+--
+-- select
+--     setval('seq_person_discounts', (select count(*) + 1 from person_discounts));
+SELECT count(*)=1 AS check1,
+      max("start_value") = 1 AS check2,
+      max("last_value") > 5 AS check3
+FROM pg_sequences
+WHERE sequencename = 'seq_person_discounts'
